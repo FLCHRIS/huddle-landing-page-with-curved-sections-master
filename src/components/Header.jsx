31b8ef2e7e64fd9e7687a4c4722618bb6378349c
Christian <filetelopezchristian@gmail.com>
2023-05-21 12:00:18 -0500

    :monkey:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,6 +40,7 @@ export const Header = () => {
           initial={{ opacity: 0, x: 200 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
+          alt=''
         />
         <div className='my-[7.6rem] md:grid md:grid-flow-col'>
           <div className='w-max mx-auto'>
